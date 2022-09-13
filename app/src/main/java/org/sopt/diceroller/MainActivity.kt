b5a939054c28d46cfa10cd65428d2bfc8f9daf80
youngjinc <cyjin6789@gmail.com>
2022-09-13 19:11:30 +0900

    [feat] 각종 뷰를 포함한 레이아웃 구현

--- app/src/main/java/org/sopt/diceroller/MainActivity.kt
+++ app/src/main/java/org/sopt/diceroller/MainActivity.kt
@@ -1,41 +1,35 @@
 package org.sopt.diceroller
 
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
-import android.widget.ImageView
-import android.widget.Toast
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    lateinit var diceImage: ImageView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val rollButton: Button = findViewById(R.id.roll_button)
-        rollButton.apply {
-            text = "Let's Roll"
-            setOnClickListener {
-                rollDice()
-                Toast.makeText(this@MainActivity, "button clicked", Toast.LENGTH_SHORT).show()
-            }
-
-            diceImage.findViewById<ImageView>(R.id.dice_image)
+        findViewById<Button>(R.id.done_button).setOnClickListener {
+            addNickname(it)
         }
     }
 
-    private fun rollDice() {
-        val randomInt = Random().nextInt(6) + 1
-        val drawableResource = when (randomInt) {
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            else -> R.drawable.dice_6
-        }
-        diceImage.setImageResource(drawableResource)
+    private fun addNickname(view: View) {
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+
+        nicknameTextView.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
+
+        // Hide the keyboard.
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
