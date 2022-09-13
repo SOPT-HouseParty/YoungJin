fc376a027f13e4692c61338ace455978397335b1
youngjinc <cyjin6789@gmail.com>
2022-09-13 16:26:02 +0900

    [feat] findViewById를 사용한 주사위 예제 구현

--- app/src/main/java/org/sopt/diceroller/MainActivity.kt
+++ app/src/main/java/org/sopt/diceroller/MainActivity.kt
@@ -1,11 +1,38 @@
 package org.sopt.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.apply {
+            text = "Let's Roll"
+            setOnClickListener {
+                rollDice()
+                Toast.makeText(this@MainActivity, "button clicked", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun rollDice() {
+        val randomInt = Random().nextInt(6) + 1
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
     }
 }
